331efd76632892785e8ea6d997e3922074750450
YaBoiSeth <94728229+YaBoiSeth@users.noreply.github.com>
2025-05-19 16:22:07 -0400

    Update main.cpp

--- ProjectCode/main.cpp
+++ ProjectCode/main.cpp
@@ -5,10 +5,17 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Create the main window
+    
     MainWindow w;
+
+     //Shows the parent being the main window, which automatically shows the children
     w.show();
+
+    
     return a.exec();
 }
 
 
-//Shows the parent being the main window, which automatically shows the children
+
